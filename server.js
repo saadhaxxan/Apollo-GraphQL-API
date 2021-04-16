9bf69f53e1e5c292c3783488a7dc3f67c81d921c
Saad Hassan <saad.haxxan786@gmail.com>
2021-04-16 19:31:16 +0500

    added basic GraphQL server

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ async function startSever() {
 
   await apolloServer.start();
   apolloServer.applyMiddleware({ app: app });
-  app.listen(4000);
+  app.listen(4000, () => console.log("Listening on 4000"));
 }
 
 startSever();
